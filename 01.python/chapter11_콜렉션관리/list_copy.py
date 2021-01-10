2a4a21f0ae9f0f1106ebf74c19d1203f7d642182
MinjiKim <kimminji1013@webmail.korea.ac.kr>
2021-01-11 00:00:01 +0900

    commit_0110

--- 01.python/chapter11_콜렉션관리/list_copy.py
+++ 01.python/chapter11_콜렉션관리/list_copy.py
@@ -15,7 +15,7 @@ list2[1] = 100
 print(list1)
 print(list2)
 print(list1 == list2)
-
+"""
 # 문자 아니고 문자열!에 대한 것임. 
 # str의 element 아니고, 참조하는 변수를 바꿀 땐 list2만 바뀜.
 str = 'Hi im minji'
@@ -25,11 +25,11 @@ str2 = "Bye"
 #str2[0] = 'h'    #----> 문자열에서는 element 기능을 수행하지 않음. 
 print(str, str2)   #----> 참조 요소가 아니라 참조 변수(화살표) 자체가 가리키는 메모리 변수가 바뀐 것.
                     #따라서 str2 만 바뀜.
-"""
+
 
 
 # 얕은 복사, 깊은 복사
-
+"""
 import copy # 깊은 복사의 copy를 위해
 list0 = ['a','b']
 list1 = [list0,1,2]
@@ -45,3 +45,4 @@ list5 =copy.deepcopy (list4) # 깊은 복사
 list5[0][1] = 'c'
 print(list4)
 print(list5)
+"""
